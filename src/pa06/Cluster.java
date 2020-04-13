819226e9b4e1cc26721e9875cf65ed64d35d936b
adomisodo <55893688+adomisodo@users.noreply.github.com>
2020-04-13 16:44:48 -0400

    add name
    add name in cluster

--- src/pa06/Cluster.java
+++ src/pa06/Cluster.java
@@ -4,6 +4,7 @@ package pa06;
  * A cluster is a cluster point (which is itself a sample)
  * and a list of Samples (the one's closest to that sample point, ideally).
  * @author presenting
+ * Jiaying
  *
  */
 public class Cluster {
