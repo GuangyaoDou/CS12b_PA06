332e2c4d06df93f23685e2c00ddfcfa9a9848330
GuangyaoDou <58749007+GuangyaoDou@users.noreply.github.com>
2020-04-14 21:28:48 -0700

    Added method to initialize cluster points

--- src/pa06/Cluster.java
+++ src/pa06/Cluster.java
@@ -1,5 +1,8 @@
 package pa06;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * A cluster is a cluster point (which is itself a sample)
  * and a list of Samples (the one's closest to that sample point, ideally).
@@ -8,5 +11,31 @@ package pa06;
  *
  */
 public class Cluster {
+	ArrayList<Sample> cluster;
+	Sample Cluster_Point;
+	
+	public Cluster(ArrayList<Sample> s) {
+		cluster = new ArrayList<Sample>();
+		for(int i = 0; i<s.size();i++) {
+			cluster.add(s.get(i));
+		}
+	}
+	
+	/**
+	 * Returns an ArrayList of randomly chosen cluster points used when we initiallze K means algorithim
+	 * @param k is the number of cluster points we are looking for in a given cluster
+	 * @return
+	 */
+	public ArrayList<Sample> Centroid(int k){
+		Random random = new Random();
+		ArrayList<Sample> centroid = new ArrayList<Sample>();
+		while(centroid.size()!= k) {
+			int ran = random.nextInt(cluster.size());
+			if(!centroid.contains(cluster.get(ran))){
+				centroid.add(cluster.get(ran));
+			}
+		}
+		return centroid;
+	}
 
 }
